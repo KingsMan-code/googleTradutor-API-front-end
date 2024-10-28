d628da93fa6d0988d46533af0b438925a756733e
Matheus Figueiredo <matheus.figueiredo.prestserv@petrobras.com.br>
2024-10-28 10:43:19 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'background': '#F1F3F4',
+        'secondaryBackground': '#F5F5F5',
+        'headerColor': '#5F6368',
+        'textColor': '#3C4043',
+      },
+    },
+  },
+  plugins: [],
+}
